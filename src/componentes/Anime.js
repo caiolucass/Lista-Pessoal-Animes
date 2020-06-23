56baeb6658d99bd12e7b31a06d705e284bbe7243
Caio Lucas <caiolfsantoss@gmail.com>
2020-06-23 11:24:39 -0300

    novos botoes e funcionalidades

--- src/componentes/Anime.js
+++ src/componentes/Anime.js
@@ -1,18 +1,46 @@
 import React, { Component } from "react";
 
-/*Animes*/
+/*Construtor dos Animes*/
 export default class Anime extends Component {
-  ApertaBotao() {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  //Adicionar Anime à lista
+  AdicionarAnime = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  //Resetar a lista de Animes
+  ResetarAnime = () => {
+    this.setState({ count: 0 });
+  };
+
   render() {
-    const { img, anime, autor } = this.props.info;
+    const { id, img, anime, autor } = this.props.info;
+    const { ExcluirAnime } = this.props; //acessar as propriedaades
+
     return (
       <article className="anime">
         <img src={img} wdith="150" alt="animes favoritos"></img>
         <div className="teste">
           <h3>Anime: {anime}</h3>
           <h5>Autor: {autor}</h5>
-          <button type="button" onClick={this.ApertaBotao}>
-            Assistir Anime
+          <h6>Carrinho: {this.state.count}</h6>
+
+          <button type="button" onClick={this.AdicionarAnime}>
+            Adicionar Anime
+          </button>
+
+          <button type="button" onClick={() => ExcluirAnime(id)}>
+            Excluir Anime
+          </button>
+
+          <button type="button" onClick={this.ResetarAnime}>
+            Resetar Anime
           </button>
         </div>
       </article>
--- src/componentes/ListaAnime.js
+++ src/componentes/ListaAnime.js
@@ -10,12 +10,21 @@ export default class ListaAnime extends Component {
       animes: dadosAnime,
     };
   }
+
+  //Acessar a lista de animes e filtrar os animes pelo id,
+  ExcluirAnime = (id) => {
+    const ArrayAnimes = this.state.animes.filter((item) => item.id !== id);
+    this.setState({
+      animes: ArrayAnimes,
+    });
+  };
+
   render() {
     return (
       <section>
         <h3>Minha Lista de Animes Favoritos</h3>
         {this.state.animes.map((item) => (
-          <Anime key={item.id} info={item} />
+          <Anime key={item.id} info={item} ExcluirAnime={this.ExcluirAnime} />
         ))}
       </section>
     );
